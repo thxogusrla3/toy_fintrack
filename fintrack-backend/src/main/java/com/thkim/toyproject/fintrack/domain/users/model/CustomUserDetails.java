69113e0b9964a14f886aa87b1effd4ee888d1af8
김태현 <thxogusrla@gmail.com>
2025-09-23 22:36:26 +0900

    refactor(folder): reorganize folder structure and class to record

--- fintrack-backend/src/main/java/com/thkim/toyproject/fintrack/domain/users/model/CustomUserDetails.java
+++ fintrack-backend/src/main/java/com/thkim/toyproject/fintrack/domain/users/model/CustomUserDetails.java
@@ -1,4 +1,4 @@
-package com.thkim.toyproject.fintrack.domain.users;
+package com.thkim.toyproject.fintrack.domain.users.model;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -7,12 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-public class CustomUserDetails implements UserDetails {
-    private final User user;
-
-    public CustomUserDetails(User user) {
-        this.user = user;
-    }
+public record CustomUserDetails(User user) implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
